132327ad0bddd662d1e41f2c57ec5186fe6d06b3
Gabe <36457852+Unclechamps@users.noreply.github.com>
2018-05-29 18:58:08 -0500

    to do app

---
+++ todo.js
@@ -0,0 +1,37 @@
+let taskList =  $('#tasks')
+let completedItems = $('#completedItems')
+
+$('#addbtn').click(function(){
+  let title = $('#userInput').val();
+  let liItem = $("<li>")
+  let checkbox = $('<input>').attr("type","checkbox").addClass("check")
+  let delBtn = $("<button>").html('Delete')
+
+  $(liItem).append(checkbox,title, delBtn)
+
+  $(taskList).append(liItem)
+  $('#userInput').val("")
+
+  delBtn.click(function() {
+    $(this).parent().remove()
+
+})
+
+  checkbox.change(function () {
+    if($(this).is(":checked")) {
+    let li = $(this).parent()
+    completedItems.append(li)
+  } else {
+    let li = $(this).parent()
+    taskList.append(li)
+  }
+
+
+})
+
+$(function() {
+  $('.sortable').sortable();
+
+  $('.sortable').disableSelection();
+});
+})
